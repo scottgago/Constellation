ea54474e5eac1bad514cbb80ddfcfcd87ff0b95c
scottgago <scottgago@gmail.com>
2016-06-21 16:51:02 -0700

    fixing merge conflicts

--- client/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import AdminReducer from './admin.reducer';
-
-const rootReducer = combineReducers({
-  posts: AdminReducer
-});
-
-export default rootReducer;
