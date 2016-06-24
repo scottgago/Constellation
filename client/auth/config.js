9cef3af2fdf90f5ac2bf05bd9839abf2eed5601e
Rong <rongster3544@gmail.com>
2016-06-24 09:26:42 -0700

    [feature] created signin component, auth actions, auth router

---
+++ client/auth/config.js
@@ -0,0 +1,13 @@
+export const FIREBASE_CONFIG = {
+  apiKey: 'AIzaSyDjiSH2Ta4-jYLkJ32U3F7JC6nvcIeJLqU',
+  authDomain: 'todo-865bf.firebaseapp.com',
+  databaseURL: 'https://todo-865bf.firebaseio.com',
+  storageBucket: 'todo-865bf.appspot.com'
+};
+
+
+// Route paths
+export const SIGN_IN_PATH = '/sign-in';
+export const TASKS_PATH = '/tasks';
+export const POST_SIGN_IN_PATH = TASKS_PATH;
+export const POST_SIGN_OUT_PATH = SIGN_IN_PATH;
